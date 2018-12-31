c2839c19f718b150af16ab5a73d0103f8b0f3adf
graycadeau <g.zawadi.w.gmail.com>
2018-12-31 14:47:33 +0300

    feature(frontend):integrate frontend
    -integrate frontend
    
    [feature #162893494]

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
+  entry: "./src/index.js",
   module: {
     rules: [
       {
@@ -7,7 +15,26 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [htmlWebpackPlugin]
 };
